ee91ff92fee016f30aeba3c1645e2b127180657e
gmeriano <gvmeriano@gmail.com>
2025-07-13 11:58:46 -0500

    smoothing off, no jitter, less smooth camera

--- scripts/misc/camera_2d.gd
+++ scripts/misc/camera_2d.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-@export var normal_camera_zoom = 1.5
+@export var normal_camera_zoom = 1.0
 @export var dimension = 1  # 1 or 2
 @export var edge_threshold: float = 0.0
 
@@ -9,9 +9,12 @@ var player2: Player = GameManager.get_player_2()
 
 var initial_position: Vector2
 
+const CAMERA_LERP_SPEED := 100.0  # Pixels per second
+
 func _ready() -> void:
 	zoom = Vector2(normal_camera_zoom, normal_camera_zoom)
-	initial_position = global_position
+	initial_position = position.round()
+	position_smoothing_enabled = false  # Disable built-in smoothing
 	
 	limit_left = 0
 	if dimension == 1:
@@ -20,12 +23,11 @@ func _ready() -> void:
 		initial_position.y += Global.DIMENSION_OFFSET
 		limit_bottom = Global.DIMENSION_OFFSET
 
-func _physics_process(delta: float) -> void:
-	call_deferred("_update_camera_logic", delta)
+func _process(delta: float) -> void:
+	_update_camera_logic(delta)
+	#call_deferred("_update_camera_logic", delta)
 
 func _update_camera_logic(delta: float) -> void:
-	var camera_pos = get_screen_center_position()
-
 	if edge_threshold == 0:
 		edge_threshold = get_viewport_rect().size.x * 0.33
 
@@ -34,12 +36,12 @@ func _update_camera_logic(delta: float) -> void:
 		set_x_position(delta)
 
 func reset() -> void:
-	global_position = initial_position.round()
+	position = initial_position.round()
 
 func set_x_position(delta: float) -> void:
 	var viewport_width = get_viewport_rect().size.x / zoom.x
 	var half_width = viewport_width * 0.5
-	var camera_pos_x = global_position.x
+	var camera_pos_x = position.x
 	
 	var left_edge = camera_pos_x - half_width + edge_threshold
 	var right_edge = camera_pos_x + half_width - edge_threshold
@@ -47,10 +49,18 @@ func set_x_position(delta: float) -> void:
 	var p1_x = player1.global_position.x
 	var p2_x = player2.global_position.x
 
-	# Only move camera if necessary
 	if p1_x < left_edge or p1_x > right_edge or p2_x < left_edge or p2_x > right_edge:
 		var mid_x = (p1_x + p2_x) * 0.5
-		global_position.x = lerp(global_position.x, mid_x, 1.0 * delta)
+
+		# Smooth toward midpoint
+		position.x = move_toward(position.x, mid_x, CAMERA_LERP_SPEED * delta)
+
+		# Snap if close enough
+		if abs(position.x - mid_x) < 1.0:
+			position.x = round(mid_x)
+
+	# Final pixel alignment
+	#position.x = round(position.x)
 
 func get_active_player() -> Player:
 	return player1 if player1.current_dimension == dimension else player2
--- scripts/misc/sub_viewport.gd
+++ scripts/misc/sub_viewport.gd
@@ -3,3 +3,6 @@ extends SubViewport
 
 func _ready() -> void:
 	set_size(Vector2i(DisplayServer.window_get_size().x, DisplayServer.window_get_size().y / 2.0))
+	print("SIZE: ", size)
+	snap_2d_transforms_to_pixel = true
+	render_target_update_mode = SubViewport.UPDATE_ALWAYS
