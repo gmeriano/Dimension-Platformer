9f0f829878547d7014d03b16c3466d3c469e38d1
gmeriano <gvmeriano@gmail.com>
2025-07-16 01:51:34 -0500

    updates

--- scripts/misc/camera_2d.gd
+++ scripts/misc/camera_2d.gd
@@ -14,7 +14,7 @@ const CAMERA_LERP_SPEED := 300.0  # Pixels per second
 func _ready() -> void:
 	zoom = Vector2(normal_camera_zoom, normal_camera_zoom)
 	initial_position = position
-	position_smoothing_enabled = false  # Disable built-in smoothing
+	#position_smoothing_enabled = false  # Disable built-in smoothing
 	
 	limit_left = 0
 	if dimension == 1:
@@ -23,7 +23,7 @@ func _ready() -> void:
 		initial_position.y += Global.DIMENSION_OFFSET
 		limit_bottom = Global.DIMENSION_OFFSET
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	_update_camera_logic(delta)
 	#call_deferred("_update_camera_logic", delta)
 
@@ -42,7 +42,7 @@ func set_x_position(delta: float) -> void:
 	var viewport_width = get_viewport_rect().size.x / zoom.x
 	var half_width = viewport_width * 0.5
 	var camera_pos_x = position.x
-	
+
 	var left_edge = camera_pos_x - half_width + edge_threshold
 	var right_edge = camera_pos_x + half_width - edge_threshold
 
@@ -51,16 +51,10 @@ func set_x_position(delta: float) -> void:
 
 	if p1_x < left_edge or p1_x > right_edge or p2_x < left_edge or p2_x > right_edge:
 		var mid_x = (p1_x + p2_x) * 0.5
-
-		# Smooth toward midpoint
 		position.x = move_toward(position.x, mid_x, CAMERA_LERP_SPEED * delta)
-		print("POS: ", position.x)
-		# Snap if close enough
-		#if abs(position.x - mid_x) < 1.0:
-			#position.x = round(mid_x)
 
-	# Final pixel alignment
-	#position.x = round(position.x)
+	# Final pixel snap
+	position.x = round(position.x)
 
 func get_active_player() -> Player:
 	return player1 if player1.current_dimension == dimension else player2
--- scripts/misc/sub_viewport.gd
+++ scripts/misc/sub_viewport.gd
@@ -6,5 +6,5 @@ func _ready() -> void:
 	#set_size(Vector2i(DisplayServer.window_get_size().x, DisplayServer.window_get_size().y / 2.0))
 	set_size(Vector2i(256, 144/2))
 	print("SIZE: ", size)
-	#snap_2d_transforms_to_pixel = true
+	snap_2d_transforms_to_pixel = true
 	render_target_update_mode = SubViewport.UPDATE_ALWAYS
