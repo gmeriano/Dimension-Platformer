41625f42d1cf02754dc451587df8aa6a9388402b
gmeriano <gvmeriano@gmail.com>
2025-07-19 14:27:11 -0500

    updated parallax node

--- scenes/background/parallax_background.gd
+++ scenes/background/parallax_background.gd
@@ -11,6 +11,9 @@ class_name GameBackgroundParallaxNode
 @export var foreground_texture: Texture2D
 @export var foreground_speed: float
 @export var foreground_repeat: int
+@export var special_texture: Texture2D
+@export var special_speed: float
+@export var special_repeat: int
 @export var image_size: Vector2
 
 @onready var parallax_2d: Parallax2D = $Parallax2D
@@ -19,17 +22,10 @@ class_name GameBackgroundParallaxNode
 @onready var secondary_background: Sprite2D = $Parallax2D2/secondary_background
 @onready var parallax_2d_3: Parallax2D = $Parallax2D3
 @onready var foreground: Sprite2D = $Parallax2D3/foreground
+@onready var parallax_2d_4: Parallax2D = $Parallax2D4
+@onready var special: Sprite2D = $Parallax2D4/special
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#if dimension == 1:
-		#global_position = GameManager.camera1.get_screen_center_position()
-	#if dimension == 2:
-		#var pos = GameManager.camera1.get_screen_center_position()
-		#global_position.y += Global.DIMENSION_OFFSET
-		#global_position = pos
-	print("POS: ", global_position)
 	parallax_2d.scroll_scale.x = background_speed
 	parallax_2d.repeat_size = image_size
 	parallax_2d.repeat_times = background_repeat
@@ -39,7 +35,12 @@ func _ready() -> void:
 	parallax_2d_3.scroll_scale.x = foreground_speed
 	parallax_2d_3.repeat_size = image_size
 	parallax_2d_3.repeat_times = foreground_repeat
-	
+	parallax_2d_4.scroll_scale.x = special_speed
+	parallax_2d_4.repeat_size = image_size
+	parallax_2d_4.repeat_times = special_repeat
+
 	background.texture = background_texture
 	secondary_background.texture = secondary_background_texture
 	foreground.texture = foreground_texture
+	special.texture = special_texture
+
