2c9d37988544a5d74b5ec567de6bc554f48e360a
gmeriano <gvmeriano@gmail.com>
2025-07-25 00:16:33 -0500

    added BackgroundNode for dynamic parallax backgrounds

---
+++ assets/resources/background_settings.gd
@@ -0,0 +1,6 @@
+class_name BackgroundSettings extends Resource
+
+@export var image_scale: float = 1.0
+@export var texture: Texture2D
+@export var speed: float
+@export var repeat: int
---
+++ scenes/levels/background/background_node.gd
@@ -0,0 +1,35 @@
+@tool
+extends Node2D
+class_name BackgroundNode
+
+@export var background_settings: Array[BackgroundSettings]
+@export var image_size: Vector2
+
+var background_set = false
+
+func _ready() -> void:
+	if !background_set:
+		background_set = true
+		set_background_from_settings()
+
+func _process(_delta: float) -> void:
+	if Engine.is_editor_hint() and !background_set:
+		print(background_set)
+		background_set = true
+		print("Setting background from settings in editor.")
+		set_background_from_settings()
+
+func set_background_from_settings() -> void:
+	z_index = -1000  # Ensure the background is rendered behind everything else
+	position = Vector2(image_size.x / 2.0, -image_size.y / 2.0)
+	for i in range(background_settings.size()):
+		var parallax_2d = Parallax2D.new()
+		parallax_2d.scroll_scale.x = background_settings[i].speed
+		parallax_2d.repeat_size = Vector2(image_size.x, 0)
+		parallax_2d.repeat_times = background_settings[i].repeat
+		var texture_sprite = Sprite2D.new()
+		texture_sprite.texture = background_settings[i].texture
+		texture_sprite.scale = Vector2(background_settings[i].image_scale, background_settings[i].image_scale)
+		parallax_2d.add_child(texture_sprite)
+		add_child(parallax_2d)
+
