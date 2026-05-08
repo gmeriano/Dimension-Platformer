1518d8a997809364173434fa2477548f05492949
gmeriano <gvmeriano@gmail.com>
2026-05-07 22:00:00 -0500

    finished intro trampoline level

--- scenes/levels/game_levels/intro_trampoline_level.gd
+++ scenes/levels/game_levels/intro_trampoline_level.gd
@@ -2,9 +2,10 @@ extends Node2D
 @onready var button: GameButton = $Dimension2/Button
 @onready var moving_platform_spawn: Marker2D = $Dimension1/MovingPlatformSpawn
 
-const MovingPlatformScene = preload("res://scenes/objects/moving_platform/moving_platform.tscn")
+const MovingPlatformScene = preload("res://scenes/objects/moving_platform_v2/MovingPlatformV2.tscn")
 
 var has_button_been_pressed = false
+var moving_platform_instance = null
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,11 +13,15 @@ func _ready() -> void:
 	button.button_pressed.connect(_on_button_pressed)
 
 func _on_button_pressed() -> void:
-	if !has_button_been_pressed:
+	if has_button_been_pressed:
+		moving_platform_instance.queue_free()
+		has_button_been_pressed = false
+	else:
 		has_button_been_pressed = true
-		var moving_platform = MovingPlatformScene.instantiate()
-		moving_platform.global_position = moving_platform_spawn.global_position
-		moving_platform.despawn = true
-		moving_platform.speed = 90.0
-		add_child(moving_platform)
-		moving_platform.can_move = true
+		moving_platform_instance = MovingPlatformScene.instantiate()
+		moving_platform_instance.global_position = moving_platform_spawn.global_position
+		moving_platform_instance.distance = 1500
+		moving_platform_instance.time = 25
+		moving_platform_instance.start_right = true
+		add_child(moving_platform_instance)
+		
