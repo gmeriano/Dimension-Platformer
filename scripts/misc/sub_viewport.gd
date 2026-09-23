22d63fcfc8cf95e6019ff1831e9819b1b4ffd3e2
gmeriano <gvmeriano@gmail.com>
2026-09-23 16:38:30 -0500

    turn off 2d snap

--- scripts/misc/sub_viewport.gd
+++ scripts/misc/sub_viewport.gd
@@ -7,5 +7,5 @@ func _ready() -> void:
 	#set_size(Vector2i(640, 180))
 	set_size(Vector2i(1920, 1080/2.0))
 	print("SIZE: ", size)
-	snap_2d_transforms_to_pixel = true
+	#snap_2d_transforms_to_pixel = true
 	render_target_update_mode = SubViewport.UPDATE_ALWAYS
